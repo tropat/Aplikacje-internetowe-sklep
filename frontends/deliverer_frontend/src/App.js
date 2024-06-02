7c61a0cc40b7eef1d6f564b5d7b72dfd6e8a6979
frieZZerr <sebastian.seczyk@gmail.com>
2024-06-02 16:13:07 +0200

    Create dockerfiles with docker-compose

--- frontends/deliverer_frontend/src/App.js
+++ frontends/deliverer_frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './interfaces/HomePage';
