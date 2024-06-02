3110089145f0d8eaef944742aba5902248addad3
Pat <patrycjatr2001@gmail.com>
2024-06-02 18:00:31 +0200

    check role while logging in

--- microservices/auth_microservice/controllers/userController.js
+++ microservices/auth_microservice/controllers/userController.js
@@ -53,7 +53,7 @@ const getAuth = async (req, res) => {
 
     const accessToken = _generateAccessToken(user);
 
-    res.status(200).json({ message: 'Authentication successful', accessToken });
+    res.status(200).json({ message: 'Authentication successful', accessToken: accessToken });
   } catch (error) {
     res.status(500).json({ error: 'Authentication unsuccessful' });
   }
